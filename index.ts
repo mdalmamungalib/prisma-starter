5520c90401943655ae4669508e54366faa853490
MD Al Mamun Galib <108384550+mdalmamungalib@users.noreply.github.com>
2025-11-22 11:31:47 +0600

    data push

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/users", async (_, res) => {
+  const users = await prisma.user.findUnique({
+    where: { email: "galib@example.com" },
+  });
+  res.json(users);
 });
 
 app.listen(PORT, () => {
